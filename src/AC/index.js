520fbadb24acb30403f056c4e550f498824b7fd2
andrew.petin <andrew.petin@effective-soft.com>
2018-06-01 13:49:44 +0300

    add axios

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,8 +1,20 @@
 import {SEARCH_CITY_INFO} from '../constants';
+import axios from 'axios';
+
 
 export function cityInfoSearch(city) {
+  const APIUrl = 'http://api.openweathermap.org/data/2.5/forecast';
+  const appId = '60c7496f70c0fd25c146a30b2ddb3702';
+
+  const request = axios.get(APIUrl, {
+    params: {
+      APPID: appId,
+      q: `${city},us`
+    }
+  });
+
   return {
     type: SEARCH_CITY_INFO,
-    payload: { city }
+    payload: request
   };
 }
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,5 @@
 export const SEARCH_CITY_INFO = 'SEARCH_CITY_INFO';
+
+export const START = '_START';
+export const SUCCESS =  '_SUCCESS';
+export const FAIL = '_FAIL';
